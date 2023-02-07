04c6307502ceec80bad89fbf8783cadc5937c856
Your Name <you@example.com>
2023-02-07 15:25:18 -0600

    Fixed misspelling in FormField, finishing rendering everything for CreatePost

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormField = ({ LabelName, type, name, placeholder, value, handleChnage, isSurpriseMe, handleSurpriseMe}) => {
+const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe}) => {
   return (
     <div>
       <div className='flex items-center gap-2 mb-2'>
@@ -26,7 +26,7 @@ const FormField = ({ LabelName, type, name, placeholder, value, handleChnage, is
         name={name}
         placeholder={placeholder}
         value={value}
-        //onChange={handleChange}
+        onChange={handleChange}
         required
         className='bg-gray-50 border sborder-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
       /> 
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,13 +20,21 @@ const CreatePost = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {}
+  const generateImage = () => {
+    
+  }
 
   const handleSubmit = () => {}
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-  const handleSurpriseMe = () => {}
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
   return (
     <section className='max-w-7xl mx-auto'>
    <div>
