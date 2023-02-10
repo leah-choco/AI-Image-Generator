c0d6c1994ed0ca0adcd7de7b504773781925afed
Your Name <you@example.com>
2023-02-10 10:45:30 -0600

    Added route to get all posts. Back-end complete

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -16,7 +16,15 @@ cloudinary.config({
 });
 
 //This route GETs all posts
-router.route("/").get(async (req, res) => {});
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
 
 //This route CREATES a post
 router.route("/").post(async (req, res) => {
