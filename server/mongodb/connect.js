66f4b3c0246f5b44946d156b47e1bb6142a00879
Your Name <you@example.com>
2023-02-11 10:49:41 -0600

    Adding connection string

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -4,7 +4,10 @@ const connectDB = (url) => {
   mongoose.set("strictQuery", true);
 
   mongoose
-    .connect(url)
+    .connect(process.env.MONGODB_URI || "http://localhost:8080", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
 };
